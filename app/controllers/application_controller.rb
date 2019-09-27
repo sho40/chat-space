2edcf4be215204040c8ed3ac34ab65df5aa03e11
sho40 <kanazawa.sho40@gmail.com>
2019-09-27 18:39:16 +0900

    define users_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :authenticate_user!
+  before_action :configur_permited_parameters, if: :devise_controller?
+
+  def configur_permited_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
