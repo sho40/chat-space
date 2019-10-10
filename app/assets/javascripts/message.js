831500648620a4bee695cfbb3aeebb85bcc6644e
sho40 <kanazawa.sho40@gmail.com>
2019-10-10 16:05:33 +0900

    create message.js file

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,47 @@
+$(function(){
+  function buildHTML(message) {
+    var content = message.content ? `${ message.content }` : "";
+    var img = message.image ? `<img src= ${ message.image }>` : "";
+    var html = `<div class="message" data-id="${message.id}">
+                  <div class="message__upper-info">
+                    <p class="message__upper-info__talker">
+                      ${message.user_name}
+                    </p>
+                    <p class="message__upper-info__date">
+                      ${message.date}
+                    </p>
+                  </div>
+                  <p class="message__text">
+                    <div>
+                    ${content}
+                    </div>
+                    ${img}
+                  </p>
+                </div>`
+  return html;
+  }
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "post",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.message_content').val('')
+    })
+    .fail(function(data){
+      alert('エラーが発生したためメッセージは送信できませんでした。');
+    })
+    .always(function(data){
+      $('.submit-btn').prop('disabled', false);
+    })
+  })
+});
